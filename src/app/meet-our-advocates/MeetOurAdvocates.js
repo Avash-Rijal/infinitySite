3e37b4bc97cbfb36b95451ce03e12f5f58cddac9
Avash Rijal <rijalavash46@gmail.com>
2024-08-30 01:06:42 +0545

    Responsive remaining and some pages too

---
+++ src/app/meet-our-advocates/MeetOurAdvocates.js
@@ -0,0 +1,46 @@
+import "./globals.css";
+
+export default function MeetOurAdvocates() {
+    let advocates_list = [
+        {
+            name:"Narayan Chaulagain",
+            image:"",
+            designation:"Managing Partner",
+        },
+        {
+            name:"Lekha Nath Bhattarai",
+            image:"",
+            designation:"Managing Partner",
+        },
+        {
+            name:"Pratima Kanel Kharel",
+            image:"",
+            designation:"Founding Partnerr",
+        },
+        {
+            name:"Astha Poudel",
+            image:"",
+            designation:"Associate",
+        },
+        {
+            name:"Gyanu Tamang",
+            image:"",
+            designation:"Associate",
+        },
+        {
+            name:"Rejina Paudyal",
+            image:"",
+            designation:"Associate",
+        },
+        {
+            name:"Aashma Acharya",
+            image:"",
+            designation:"Associate",
+        },
+    ]
+    return (
+    <div className="container">
+        
+    </div>
+    );
+}
